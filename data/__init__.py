2aff785703cd65530b80aa72492184ba74888371
Paul Shin <toyaji83@gmail.com>
2021-10-01 13:29:00 +0900

    lightning datamodule complete

--- data/__init__.py
+++ data/__init__.py
@@ -2,58 +2,45 @@ from pytorch_lightning import LightningDataModule
 from torch.utils.data import DataLoader, ConcatDataset
 
 from typing import Optional
-
-from .div2k import DIV2K
-from .bsd500 import BSD500
-from .zoomdata import ZoomLZoomData
-from .zoomraw import ZoomLZoomRaw
+from importlib import import_module
 
 class LitDataset(LightningDataModule):
     def __init__(self,
-                 dir,
+                 args,
                  data='zoom',
-                 patch_size=64,
                  batch_size=4,
-                 scale_idx=[2, 3, 4],
                  shuffle=True,
-                 num_workers=4, 
+                 num_workers=4,
+                 test_only=False,
                  **kwargs):
                  
         super().__init__()
 
         self.data = data
-        self.dir = dir
-        self.scale_idx = scale_idx
-        self.patch_size = patch_size
         self.batch_size = batch_size
         self.shuffle = shuffle
         self.num_workers = num_workers
+        self.test_only = test_only
+        self.test_data = ['Set5', 'Set14', 'BSD100', 'Urban100', 'Manga109']
 
-        # settings for zoom data
-        self.kwargs = kwargs
+        # args for dataset
+        self.args = args
 
     def setup(self, stage: Optional[str]=None) -> None:
-        if self.data == 'div':
-            train_sets = [DIV2K(self.dir, i, self.patch_size) for i in self.scale_idx]
-            val_sets = [DIV2K(self.dir, i, self.patch_size, train=False) for i in self.scale_idx]
-        elif self.data == 'zoom':
-            train_sets = [ZoomLZoomData(self.dir, (1, i), self.patch_size, **self.kwargs) for i in self.scale_idx]
-            val_sets = [ZoomLZoomData(self.dir, (1, i), self.patch_size, train=False, **self.kwargs) for i in self.scale_idx]
-        elif self.data == 'zoomraw':
-            train_sets = [ZoomLZoomRaw(self.dir, i, self.patch_size, **self.kwargs) for i in self.scale_idx]
-            val_sets = [ZoomLZoomRaw(self.dir, i, self.patch_size, train=False, **self.kwargs) for i in self.scale_idx]
-        elif self.data == 'bsd':
-            train_sets = [BSD500(self.dir, i, self.patch_size) for i in self.scale_idx]
-            val_sets = [BSD500(self.dir, i, self.patch_size, train=False) for i in self.scale_idx]
-            test_sets = [BSD500(self.dir, i, self.patch_size, train=False, test=True) for i in self.scale_idx]
-           
-        self.train_set = ConcatDataset(train_sets)
-        self.val_set = ConcatDataset(val_sets)
+        if not self.test_only:
+            datasets = []
+            for d in self.data:
+                m = import_module('data.' + d.lower())
+                datasets.append(getattr(m, d)(**self.args, name=d))
 
-        if self.data == 'bsd':
-            self.test_set = ConcatDataset(test_sets)
-        else:
-            self.test_set = self.val_set
+        testsets = []
+        for d in self.test_data:
+            m = import_module('data.benchmark')
+            testsets.append(getattr(m, 'Benchmark')(**self.args, train=False, name=d))
+
+        self.train_set = ConcatDataset(datasets)
+        self.val_set = ConcatDataset(testsets)
+        self.test_set = ConcatDataset(testsets)
 
     def train_dataloader(self):
         return DataLoader(self.train_set, self.batch_size, self.shuffle, num_workers=self.num_workers)
@@ -61,6 +48,5 @@ class LitDataset(LightningDataModule):
     def val_dataloader(self):
         return DataLoader(self.val_set, self.batch_size, self.shuffle, num_workers=self.num_workers)
 
-    # TODO benchmark test 하도록 여 부분 수정하기
     def test_dataloader(self):
         return DataLoader(self.test_set, self.batch_size, self.shuffle, num_workers=self.num_workers)
