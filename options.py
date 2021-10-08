e6e6a1e2f264c38f03a1d1886ce3b3ba1391b3a9
paul <happytoday83@naver.com>
2021-10-08 18:16:51 +0900

    options added

--- options.py
+++ options.py
@@ -12,20 +12,23 @@ def load_config(config_file):
     return config
 
 def load_config_from_args():
+    # thouh we have template for each models, still you can add any options through belowing code
     args = argparse.ArgumentParser()
     args.add_argument("-c", "--config", required=True, help="You can see the sample yaml template in /config folder.")
     args.add_argument("-n", "--name", type=str, help="You can put the name for the experiment, this will be used for log file name.")
     args.add_argument("-p", "--patch", type=int, help="Patch size for data loader crop and model generation.")
+    args.add_argument("-b", "--batch", type=int, help="Batch size for data laoder.")
     args.add_argument("--chop_size", type=int, help="For test forward, we need to chop the input according to its memory capability.")
     args.add_argument("--test_only", action='store_true', help='set this option to test the model')
-    args.add_argument("--save_test_img", action='store_true', help='set this option to test the model')
+    args.add_argument("--save_imgs", action='store_true', help='set this option to test the model')
     args = args.parse_args(sys.argv[1:])
     
     config = load_config(args.config)
     config.log.name = args.name
     config.log.version = 'log_' + datetime.now().strftime("%y%m%d%H%M")
     config.dataset.args.patch_size = args.patch
+    config.dataset.batch_size = args.batch
     config.dataset.test_only = args.test_only
-    config.dataset.save_test_img = args.save_test_img
+    config.dataset.save_test_img = args.save_imgs
     config.model.chop_size = args.chop_size
     return config
