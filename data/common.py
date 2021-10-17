3dcc770d45db33a9434343d83083ea0a1ded2f5e
Paul Shin <toyaji83@gmail.com>
2021-10-17 14:22:01 +0900

    np2tensor func modification

--- data/common.py
+++ data/common.py
@@ -78,7 +78,10 @@ def np2Tensor(*args, rgb_range=255):
     def _np2Tensor(img):
         np_transpose = np.ascontiguousarray(img.transpose((2, 0, 1)))
         tensor = torch.from_numpy(np_transpose).float()
-        tensor.mul_(rgb_range / 255)
+
+        # normalization for rgb range
+        if rgb_range == 255:
+            tensor.mul_(1 / 255)
 
         return tensor
 
