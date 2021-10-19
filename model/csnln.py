257a26df14e5fb23b1608231a9d47c53564c4a5e
Paul Shin <toyaji83@gmail.com>
2021-10-19 19:01:45 +0900

    trimming

--- model/csnln.py
+++ model/csnln.py
@@ -7,16 +7,17 @@ def make_model(args, parent=False):
 
 #projection between attention branches
 class MultisourceProjection(nn.Module):
-    def __init__(self, in_channel,kernel_size = 3,scale=2, conv=common.default_conv):
+    def __init__(self, in_channel, kernel_size = 3, scale=2, conv=common.default_conv):
         super(MultisourceProjection, self).__init__()
         deconv_ksize, stride, padding, up_factor = {
-            2: (6,2,2,2),
-            3: (9,3,3,3),
-            4: (6,2,2,2)
+            2: (6, 2, 2, 2),
+            3: (9, 3, 3, 3),
+            4: (6, 2, 2, 2)
         }[scale]
+
         self.up_attention = CrossScaleAttention(scale = up_factor)
         self.down_attention = NonLocalAttention()
-        self.upsample = nn.Sequential(*[nn.ConvTranspose2d(in_channel,in_channel,deconv_ksize,stride=stride,padding=padding),nn.PReLU()])
+        self.upsample = nn.Sequential(*[nn.ConvTranspose2d(in_channel, in_channel, deconv_ksize, stride=stride, padding=padding), nn.PReLU()])
         self.encoder = common.ResBlock(conv, in_channel, kernel_size, act=nn.PReLU(), res_scale=1)
     
     def forward(self,x):
@@ -30,26 +31,29 @@ class MultisourceProjection(nn.Module):
 
 #projection with local branch
 class RecurrentProjection(nn.Module):
-    def __init__(self, in_channel,kernel_size = 3, scale = 2, conv=common.default_conv):
+    def __init__(self, in_channel, kernel_size = 3, scale = 2, conv=common.default_conv):
         super(RecurrentProjection, self).__init__()
         self.scale = scale
         stride_conv_ksize, stride, padding = {
-            2: (6,2,2),
-            3: (9,3,3),
-            4: (6,2,2)
+            2: (6, 2, 2),
+            3: (9, 3, 3),
+            4: (6, 2, 2)
         }[scale]
 
-        self.multi_source_projection = MultisourceProjection(in_channel,kernel_size=kernel_size,scale = scale, conv=conv)
-        self.down_sample_1 = nn.Sequential(*[nn.Conv2d(in_channel,in_channel,stride_conv_ksize,stride=stride,padding=padding),nn.PReLU()])
+        self.multi_source_projection = MultisourceProjection(in_channel, kernel_size=kernel_size, scale = scale, conv=conv)
+        self.down_sample_1 = nn.Sequential(*[nn.Conv2d(in_channel, in_channel, stride_conv_ksize, stride=stride, padding=padding),nn.PReLU()])
+
         if scale != 4:
-            self.down_sample_2 = nn.Sequential(*[nn.Conv2d(in_channel,in_channel,stride_conv_ksize,stride=stride,padding=padding),nn.PReLU()])
-        self.error_encode = nn.Sequential(*[nn.ConvTranspose2d(in_channel,in_channel,stride_conv_ksize,stride=stride,padding=padding),nn.PReLU()])
-        self.post_conv = common.BasicBlock(conv,in_channel,in_channel,kernel_size,stride=1,bias=True,act=nn.PReLU())
+            self.down_sample_2 = nn.Sequential(*[nn.Conv2d(in_channel, in_channel, stride_conv_ksize, stride=stride, padding=padding),nn.PReLU()])
+
+        self.error_encode = nn.Sequential(*[nn.ConvTranspose2d(in_channel, in_channel, stride_conv_ksize, stride=stride, padding=padding),nn.PReLU()])
+        self.post_conv = common.BasicBlock(conv, in_channel, in_channel, kernel_size, stride=1, bias=True, act=nn.PReLU())
+
         if scale == 4:
-            self.multi_source_projection_2 = MultisourceProjection(in_channel,kernel_size=kernel_size,scale = scale, conv=conv)
-            self.down_sample_3 = nn.Sequential(*[nn.Conv2d(in_channel,in_channel,8,stride=4,padding=2),nn.PReLU()])
-            self.down_sample_4 = nn.Sequential(*[nn.Conv2d(in_channel,in_channel,8,stride=4,padding=2),nn.PReLU()])
-            self.error_encode_2 = nn.Sequential(*[nn.ConvTranspose2d(in_channel,in_channel,8,stride=4,padding=2),nn.PReLU()])
+            self.multi_source_projection_2 = MultisourceProjection(in_channel, kernel_size=kernel_size, scale = scale, conv=conv)
+            self.down_sample_3 = nn.Sequential(*[nn.Conv2d(in_channel, in_channel, 8, stride=4, padding=2),nn.PReLU()])
+            self.down_sample_4 = nn.Sequential(*[nn.Conv2d(in_channel, in_channel, 8, stride=4, padding=2),nn.PReLU()])
+            self.error_encode_2 = nn.Sequential(*[nn.ConvTranspose2d(in_channel, in_channel, 8, stride=4, padding=2),nn.PReLU()])
 
 
     def forward(self, x):
@@ -57,6 +61,7 @@ class RecurrentProjection(nn.Module):
         x_down = self.down_sample_1(x_up)
         error_up = self.error_encode(x-x_down)
         h_estimate = x_up + error_up
+        
         if self.scale == 4:
             x_up_2 = self.multi_source_projection_2(h_estimate)
             x_down_2 = self.down_sample_3(x_up_2)
@@ -68,9 +73,6 @@ class RecurrentProjection(nn.Module):
 
         return x_final, h_estimate
         
-
-        
-
 
 class CSNLN(nn.Module):
     def __init__(self, args, conv=common.default_conv):
@@ -87,19 +89,14 @@ class CSNLN(nn.Module):
         self.sub_mean = common.MeanShift(args.rgb_range, rgb_mean, rgb_std)
         
         # define head module
-        m_head = [common.BasicBlock(conv, args.n_colors, n_feats, kernel_size,stride=1,bias=True,bn=False,act=nn.PReLU()),
-        common.BasicBlock(conv,n_feats, n_feats, kernel_size,stride=1,bias=True,bn=False,act=nn.PReLU())]
+        m_head = [common.BasicBlock(conv, args.n_colors, n_feats, kernel_size, stride=1, bias=True, bn=False, act=nn.PReLU()),
+                  common.BasicBlock(conv, n_feats,       n_feats, kernel_size, stride=1, bias=True, bn=False, act=nn.PReLU())]
 
         # define Self-Exemplar Mining Cell
-        self.SEM = RecurrentProjection(n_feats,scale = scale)
+        self.SEM = RecurrentProjection(n_feats, scale = scale)
 
         # define tail module
-        m_tail = [
-            nn.Conv2d(
-                n_feats*self.depth, args.n_colors, kernel_size,
-                padding=(kernel_size//2)
-            )
-        ]
+        m_tail = [nn.Conv2d(n_feats*self.depth, args.n_colors, kernel_size, padding=(kernel_size//2))]
 
         self.add_mean = common.MeanShift(args.rgb_range, rgb_mean, rgb_std, 1)
 
