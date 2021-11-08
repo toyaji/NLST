be46a679e68b1b55ba21b1fd8358f3eefeee2741
Paul Shin <toyaji83@gmail.com>
2021-11-08 10:30:35 +0900

    scan againe

---
+++ model/scan.py
@@ -0,0 +1,336 @@
+import torch
+import torch.nn as nn
+import torchvision.models.vgg as vgg
+
+from collections import namedtuple
+
+from model import common
+
+class VGG(nn.Module):
+    def __init__(self, version='vgg13', requires_grad=False):
+        super(VGG, self).__init__()
+        vgg_pretrained_features = getattr(vgg, version)(pretrained=True).features
+
+        self.slice2 = nn.Sequential()
+        self.slice3 = nn.Sequential()
+        self.slice4 = nn.Sequential()
+        self.slice5 = nn.Sequential()
+        
+        idx = {
+            'vgg11' : [2, 5, 10, 15, 20],
+            'vgg13' : [4, 9, 14, 19, 24],
+            'vgg16' : [4, 9, 16, 23, 30],
+            'vgg19' : [4, 9, 18, 27, 36],
+        }[version]
+
+        for x in range(idx[0], idx[1]):
+            self.slice2.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(idx[1], idx[2]):
+            self.slice3.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(idx[2], idx[3]):
+            self.slice4.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(idx[3], idx[4]):
+            self.slice5.add_module(str(x), vgg_pretrained_features[x])
+            
+        if not requires_grad:
+            for param in self.parameters():
+                param.requires_grad = False
+
+    def forward(self, x):
+        """
+            inputs :
+                x : input feature maps(B X 64 X H X W)
+            returns :
+                out : list of feature maps(B X 128 X H/2 X W/2, 
+                                           B X 256 X H/4 X W/4,
+                                           B X 512 X H/8 X W/8,
+                                           B X 512 X H/16 X W/16)
+        """
+        h2 = self.slice2(x)   
+        h3 = self.slice3(h2)
+        h4 = self.slice4(h3)
+        h5 = self.slice5(h4)
+
+        vgg_outputs = namedtuple(
+            "VggOutputs", ['layer2', 'layer3', 'layer4', 'layer5'])
+
+        out = vgg_outputs(h2, h3, h4, h5)
+
+        return out
+
+class VGG19(nn.Module):
+    def __init__(self, requires_grad=False):
+        super(VGG19, self).__init__()
+        vgg_pretrained_features = vgg.vgg19(pretrained=True).features
+
+        self.slice1 = nn.Sequential()
+        self.slice2 = nn.Sequential()
+        self.slice3 = nn.Sequential()
+        self.slice4 = nn.Sequential()
+        self.slice5 = nn.Sequential()
+
+        for x in range(4):
+            self.slice1.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(4, 9):
+            self.slice2.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(9, 18):
+            self.slice3.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(18, 27):
+            self.slice4.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(27, 36):
+            self.slice5.add_module(str(x), vgg_pretrained_features[x])
+            
+        if not requires_grad:
+            for param in self.parameters():
+                param.requires_grad = False
+
+    def forward(self, x):
+        """
+            inputs :
+                x : input feature maps(B X 64 X H X W)
+            returns :
+                out : list of feature maps(B X 128 X H/2 X W/2, 
+                                           B X 256 X H/4 X W/4,
+                                           B X 512 X H/8 X W/8,
+                                           B X 512 X H/16 X W/16)
+        """
+        h = self.slice2(x)   
+        h_relu2_2 = h
+        h = self.slice3(h)
+        h_relu3_4 = h
+        h = self.slice4(h)
+        h_relu4_4 = h
+        h = self.slice5(h)
+        h_relu5_4 = h
+
+        vgg_outputs = namedtuple(
+            "VggOutputs", ['relu2_2',
+                           'relu3_4', 'relu4_4', 'relu5_4'])
+        out = vgg_outputs(h_relu2_2,
+                          h_relu3_4, h_relu4_4, h_relu5_4)
+
+        return out
+
+class StackChannelAttention(nn.Module):
+    def __init__(self, extractor, in_dim=64, channel=[128, 256, 512, 512], reduction=[2, 4, 8, 8]):
+        super().__init__()
+
+        self.extractor = extractor
+        self.avg_pool = nn.ModuleList([nn.AdaptiveAvgPool2d(1)] * 4)
+        self.conv_sqeeze = nn.ModuleList()
+
+        for ch, r in zip(channel, reduction):
+            self.conv_sqeeze.append(nn.Sequential(
+                nn.Conv2d(ch, ch // r, 1, padding=0, bias=True),
+                nn.ReLU(inplace=True))
+            )
+
+        self.merge_conv = nn.Conv2d(in_dim*4, in_dim, 1, padding=0, bias=True)
+
+    def forward(self, x):
+        """
+            inputs :
+                x : input feature maps(B X C X H X W)
+            returns :
+                out : B X C X H X W
+        """
+        outs = self.extractor(x)
+        pooled = []
+        for pool, sqeeze, out in zip(self.avg_pool, self.conv_sqeeze, outs):
+            pooled.append(pool(sqeeze(out)))
+            
+        pooled = torch.cat(pooled, dim=1)
+        attn = self.merge_conv(pooled)
+
+        return x * attn
+
+class LAM_Module(nn.Module):
+    """ Layer attention module"""
+    def __init__(self, in_dim):
+        super(LAM_Module, self).__init__()
+        self.chanel_in = in_dim
+
+
+        self.gamma = nn.Parameter(torch.zeros(1))
+        self.softmax  = nn.Softmax(dim=-1)
+
+    def forward(self,x):
+        """
+            inputs :
+                x : input feature maps( B X N X C X H X W)
+            returns :
+                out : attention value + input feature
+                attention: B X N X N
+        """
+        m_batchsize, N, C, height, width = x.size()
+        proj_query = x.view(m_batchsize, N, -1)
+        proj_key = x.view(m_batchsize, N, -1).permute(0, 2, 1)
+        energy = torch.bmm(proj_query, proj_key)
+        energy_new = torch.max(energy, -1, keepdim=True)[0].expand_as(energy)-energy
+        attention = self.softmax(energy_new)
+
+        proj_value = x.view(m_batchsize, N, -1)
+        out = torch.bmm(attention, proj_value)
+        out = out.view(m_batchsize, N, C, height, width)
+
+        out = self.gamma*out + x
+        out = out.view(m_batchsize, -1, height, width)
+        return out
+
+class CSAM_Module(nn.Module):
+    """ Channel-Spatial attention module"""
+    def __init__(self, in_dim):
+        super(CSAM_Module, self).__init__()
+        self.chanel_in = in_dim
+
+
+        self.conv = nn.Conv3d(1, 1, 3, 1, 1)
+        self.gamma = nn.Parameter(torch.zeros(1))
+        #self.softmax  = nn.Softmax(dim=-1)
+        self.sigmoid = nn.Sigmoid()
+    def forward(self,x):
+        """
+            inputs :
+                x : input feature maps( B X N X C X H X W)
+            returns :
+                out : attention value + input feature
+                attention: B X N X N
+        """
+        m_batchsize, C, height, width = x.size()
+        out = x.unsqueeze(1)  # B X 1 X C X H X W
+        out = self.sigmoid(self.conv(out))
+        
+        # proj_query = x.view(m_batchsize, N, -1)
+        # proj_key = x.view(m_batchsize, N, -1).permute(0, 2, 1)
+        # energy = torch.bmm(proj_query, proj_key)
+        # energy_new = torch.max(energy, -1, keepdim=True)[0].expand_as(energy)-energy
+        # attention = self.softmax(energy_new)
+        # proj_value = x.view(m_batchsize, N, -1)
+
+        # out = torch.bmm(attention, proj_value)
+        # out = out.view(m_batchsize, N, C, height, width)
+
+        out = self.gamma*out
+        out = out.view(m_batchsize, -1, height, width)
+        x = x * out + x
+        return x
+
+## Residual Channel Attention Block (RCAB)
+class RCAB(nn.Module):
+    def __init__(
+        self, conv, extractor, n_feat, kernel_size, channels, reduction,
+        bias=True, bn=False, act=nn.ReLU(True), res_scale=1):
+
+        super(RCAB, self).__init__()
+        modules_body = []
+        for i in range(2):
+            modules_body.append(conv(n_feat, n_feat, kernel_size, bias=bias))
+            if bn: modules_body.append(nn.BatchNorm2d(n_feat))
+            if i == 0: modules_body.append(act)
+        modules_body.append(
+            StackChannelAttention(extractor, n_feat, channels, reduction))
+        self.body = nn.Sequential(*modules_body)
+        self.res_scale = res_scale
+
+    def forward(self, x):
+        res = self.body(x)
+        #res = self.body(x).mul(self.res_scale)
+        res += x
+        return res
+
+## Residual Group (RG)
+class ResidualGroup(nn.Module):
+    def __init__(self, conv, extractor, n_feat, kernel_size, channels, reduction, act, res_scale, n_resblocks):
+        super(ResidualGroup, self).__init__()
+
+        #extractor = VGG19(requires_grad=True)
+
+        modules_body = []
+        modules_body = [
+            RCAB(
+                conv, extractor, n_feat, kernel_size, channels, reduction, bias=True, bn=False, act=act, res_scale=res_scale) \
+            for _ in range(n_resblocks)]
+        modules_body.append(conv(n_feat, n_feat, kernel_size))
+        self.body = nn.Sequential(*modules_body)
+
+    def forward(self, x):
+        res = self.body(x)
+        res += x
+        return res
+
+## Stack Channel Attention Network (RCAN)
+class SCAN(nn.Module):
+    def __init__(self, args, conv=common.default_conv):
+        super(SCAN, self).__init__()
+        
+        n_resgroups = args.n_resgroups
+        n_resblocks = args.n_resblocks
+        n_feats = args.n_feats
+        kernel_size = 3
+        channels = args.channels
+        reduction = args.reduction 
+        scale = args.scale
+        
+        extractor = VGG19(requires_grad=args.extractor_train)
+        act = nn.GELU()
+        
+        # RGB mean for DIV2K
+        rgb_mean = (0.4488, 0.4371, 0.4040)
+        rgb_std = (1.0, 1.0, 1.0)
+        self.sub_mean = common.MeanShift(args.rgb_range, rgb_mean, rgb_std)
+        
+        # define head module
+        modules_head = [conv(args.n_colors, n_feats, kernel_size)]
+
+        # define body module
+        modules_body = [
+            ResidualGroup(
+                conv, extractor, n_feats, kernel_size, channels, reduction, act=act, res_scale=args.res_scale, n_resblocks=n_resblocks) \
+            for _ in range(n_resgroups)]
+
+        modules_body.append(conv(n_feats, n_feats, kernel_size))
+
+        # define tail module
+        modules_tail = [
+            common.Upsampler(conv, scale, n_feats, act=False),
+            conv(n_feats, args.n_colors, kernel_size)]
+
+        self.add_mean = common.MeanShift(args.rgb_range, rgb_mean, rgb_std, 1)
+
+        self.head = nn.Sequential(*modules_head)
+        self.body = nn.Sequential(*modules_body)
+        self.csa = CSAM_Module(n_feats)
+        self.la = LAM_Module(n_feats)
+        self.last_conv = nn.Conv2d(n_feats*11, n_feats, 3, 1, 1)
+        self.last = nn.Conv2d(n_feats*2, n_feats, 3, 1, 1)
+        self.tail = nn.Sequential(*modules_tail)
+
+    def forward(self, x):
+        x = self.sub_mean(x)
+        x = self.head(x)
+        res = x
+        #pdb.set_trace()
+        for name, midlayer in self.body._modules.items():
+            res = midlayer(res)
+            #print(name)
+            if name=='0':
+                res1 = res.unsqueeze(1)
+            else:
+                res1 = torch.cat([res.unsqueeze(1),res1],1)
+        #res = self.body(x)
+        out1 = res
+        #res3 = res.unsqueeze(1)
+        #res = torch.cat([res1,res3],1)
+        res = self.la(res1)
+        out2 = self.last_conv(res)
+
+        out1 = self.csa(out1)
+        out = torch.cat([out1, out2], 1)
+        res = self.last(out)
+        
+        res += x
+        #res = self.csa(res)
+        x = self.tail(res)
+        x = self.add_mean(x)
+
+        return x 
